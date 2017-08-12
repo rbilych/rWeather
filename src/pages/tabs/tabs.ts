6475eb64f26c2bdb5d18aa0077035abe908dc8f1
Ruslan Bilych <r.bilych@gmail.com>
2017-08-12 15:35:44 +0300

    Edit about page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { SettingsPage } from '../settings/settings';
 
@@ -11,8 +10,8 @@ import { SettingsPage } from '../settings/settings';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-	tab3Root = SettingsPage;
+	tab2Root = SettingsPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
