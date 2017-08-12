7cb25c51515dc404f1e71d41060097aa482bb04e
Ruslan Bilych <r.bilych@gmail.com>
2017-08-12 16:21:45 +0300

    Add weather provider

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class WeatherProvider {
+	apiKey = '328c648d6c5a8471';
+	url;
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+		this.url = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q/';
+  }
+
+	getWeather(city, country) {
+		return this.http.get(this.url + '/' + country + '/' + city + '.json')
+			.map(res => res.json());
+	}
+
+}
